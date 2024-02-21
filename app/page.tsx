db152e365de772c56e63b7386662149ec338400a
Kris1027 <obarzanek.work@gmail.com>
2024-02-21 20:01:10 +0100

    Update page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="bg-sky-500 h-screen flex justify-center pt-5">
-      <div className=" h-64 w-96 bg-white rounded-lg p-5 flex flex-col gap-3">
-        <div>
+    <main className="bg-sky-500 h-screen flex justify-center pt-5 items-start">
+      <div className="w-[30rem] bg-white rounded-lg p-5 flex flex-col">
+        <div className="pb-5">
           <h1 className="text-xl font-bold">Do zrobienia</h1>
           <p className="text-lg font-semibold">
             <span>2</span> zadania
@@ -14,21 +14,21 @@ export default function Home() {
               className="flex-1 border-2 border-black rounded-md"
               type="text"
             />
-            <button className="border-2 border-sky-500 text-sky-500 p-1 rounded-md">
+            <button className="border-2 border-sky-500 text-sky-500 py-1 px-2 rounded-md">
               Dodaj
             </button>
           </form>
         </div>
         <ul>
-          <li className="flex flex-col gap-2">
-            <div className="h-[1px] w-full bg-black"></div>
+          <li className="flex flex-col">
+            <div className="h-[1px] w-full bg-black my-4"></div>
             <div className="flex justify-between items-center">
               <p>Zapłać rachunki</p>
               <div className="flex gap-2">
-                <button className="border-2 border-sky-500 text-sky-500 p-1 rounded-md">
+                <button className="border-2 border-sky-500 text-sky-500 py-1 px-2 rounded-md">
                   Zrobione
                 </button>
-                <button className="border-2 border-sky-500 text-sky-500 p-1 rounded-md">
+                <button className="border-2 border-sky-500 text-sky-500 py-1 px-2 rounded-md">
                   Usuń
                 </button>
               </div>
